2c3209139146a14786f0d6e54323bfaf1a27679a
unknown <chiuyuanwu@yile.com.tw>
2024-07-18 13:32:53 +0800

    123

--- stdemo.py
+++ stdemo.py
@@ -12,7 +12,6 @@ user_input = st.text_input("請輸入一些文字:")
 # 顯示使用者輸入的內容
 st.write("你輸入的內容是:", user_input)
 
-
 # 創建一個按鈕
 if st.button('點擊我有驚喜'):
     # 按下按鈕後彈出彩帶
@@ -42,10 +41,9 @@ elif operation == "除法":
     else:
         st.error("除數不能為零")
 
-# 顯示結果
-if result is not None:
-    st.write(f"結果：{result}")
-
 # 添加一個按鈕來觸發計算
+
 if st.button("計算"):
-    st.balloons()  # 添加彩帶效果
+    if result is not None:
+        st.write(f"結果：{result}")
+        st.balloons()  # 添加彩帶效果
